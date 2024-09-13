9a5a2a40bb1aceefe37098ea4c7f71216da2fc12
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-09-14 00:12:51 +0700

    get coordinates & detail location

--- src/js/app.js
+++ src/js/app.js
@@ -1,67 +1,76 @@
 import "../../src/css/main.css";
 
-const getLocation = document.querySelector('#btn-get-location');
-const latCoordinates = document.querySelector('#lat');
-const longCoordinates = document.querySelector('#long');
-const countryCode = document.querySelector('#country-code');
-const cityName = document.querySelector('#city-name');
-const regionName = document.querySelector('#region-name');
-const countryName = document.querySelector('#country-name');
+const getLocation = $("#btn-get-location");
+const latCoordinates = $("#lat");
+const longCoordinates = $("#long");
+// const countryCode = $('#country-code');
+const cityName = $("#city-name");
+const regionName = $("#region-name");
+const countryName = $("#country-name");
 
-const locationInfo = document.querySelector('#location-info');
+const locationInfo = $("#location-info");
+const baseUrl =
+  "https://api.opencagedata.com/geocode/v1/json?key=250178f6b314472c95280f5f73d608e1";
 
-
-const baseUrl = "http://ip-api.com/json";
-
-const getCoordinates = (coordinates) => {
-  navigator.geolocation.getCurrentPosition((position)=>{
-   const lat =  position.coords.latitude;
-   const long =  position.coords.longitude;
-   coordinates(lat,long);
-  })
-}
+// Fungsi untuk mendapatkan koordinat
+const getCoordinates = (success, error) => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const long = position.coords.longitude;
+        success(lat, long);
+      },
+      (err) => {
+        console.error("Error:", err.message);
+        if (error) {
+          error(err.message);
+        }
+      },
+    );
+  } else {
+    // Jika browser tidak mendukung geolocation
+    const errorMessage = "Geolocation is not supported by this browser.";
+    console.error("Error:", errorMessage);
+    if (error) {
+      error(errorMessage); // Memanggil callback error jika disediakan
+    }
+  }
+};
 
 // Fungsi untuk render response JSON
 const renderLocationInfo = (data) => {
-  countryCode.innerText = data.countryCode;
-  cityName.innerText = data.city;
-  regionName.innerText = data.regionName;
-  countryName.innerText = data.country;
-  getCoordinates((lat,long)=>{
-    longCoordinates.innerText = long;
-    latCoordinates.innerText = lat;
-  })
-
+  getCoordinates(
+    (lat, long) => {
+  latCoordinates.text(lat);
+  longCoordinates.text(long);
+})
+  cityName.text(data.results[0].components.county);
+  regionName.text(data.results[0].components.state);
+  countryName.text(data.results[0].components.country);
 };
 
+// Event listener untuk tombol get location
+// getLocation.on("click", function() {
+  $(window).on("load",()=> {
+  // locationInfo.removeClass("invisible");
+  getCoordinates(
+    (lat, long) => {
+      fetch(`${baseUrl}&q=${lat},${long}`)
+        .then((response) => response.json())
+        .then((responseJson) => {
+          renderLocationInfo(responseJson);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    },
+    (err) => {
+      alert(
+        "Unable to retrieve your location. Please ensure location services are enabled.",
+      );
+      console.error("Geolocation error:", err); // Mencetak error yang lebih spesifik
+    },
+  );
 
-
-getLocation.addEventListener('click',()=>{
-  // 
- locationInfo.classList.remove('invisible');
-//  
-    getCoordinates((lat,long)=>{
-      // 
-      fetch(`${baseUrl}/?lat=${lat}&long=${long}`)
-      .then((response) => {
-        return response.json();
-      })
-      // 
-      .then((responseJson)=>{
-        // showResponseMessage(responseJson.message);
-        console.log(responseJson);
-        // 
-        renderLocationInfo(responseJson);
-    })
-    // 
-    .catch((error)=>{
-      console.error('Error:', error); 
-      // showResponseMessage(error);
-    })
-  })
-  alert("tengs e lot")
-    });
-
- 
-
-// /?lat=-6.4484817&long=106.7905652
+});
