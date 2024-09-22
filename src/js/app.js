01cf3f5b5b0d2f9fb3bf9224a45633b2999b05a4
Afiansyah Cahyo Wicaksono <alfinxd4@gmail.com>
2024-09-22 08:59:57 +0700

    update .js

--- src/js/app.js
+++ src/js/app.js
@@ -2,18 +2,14 @@
 import "../../src/css/main.css";
 // import CSS
 import "../../src/css/styles.css";
-// 
-// import "../assets/images/background/starry-mountain.png";
-// // 
-// import "../assets/images/weather/thunderstorm/night-storm.webp";
 
-// Mengimpor semua gambar dari folder assets/images
+// import all images from assets/images folder
 const images = require.context('../assets/images', true, /\.(png|jpe?g|gif|svg|webp)$/);
 
-// Membuat array untuk menyimpan semua gambar yang diimpor
-const importedImages = images.keys().map((key) => images(key));
+// create an array to store all imported images
+images.keys().map((key) => images(key));
 
-
+// show-hide drawer
 $('#drawer-toggle').on('click', function () {
   const drawer = $('#drawer-swipe');
   const expandedContent = $('#drawer-expanded-content');
@@ -27,66 +23,55 @@ $('#drawer-toggle').on('click', function () {
   }
 });
 
-// import "../../src/assets/fonts/sf-pro-display-regular.otf";
-// 
-// Array list of day
-// const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-// Selectors
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]; // Array list of day
+const cityName = $("#city-name");
+const countryCode = $("#country-code");
+
+const currentTemp = $("#current-temp");
+const currentHumi = $("#current-humi");
+const currentWeather = $("#current-weather");
+const currentWindSpeed = $("#current-windspeed");
+const iconCurrentWeather = $("#icon-current-weather");
+
 const localTime = $("#local-time");
-// const cityName = $("#city-name");
-// const countryName = $("#country-name");
-// const temp = $("#temp");
-// const humi = $("#humidity");
-// const weather = $("#weather");
-// const minTemp = $("#min-temp");
-// const maxTemp = $("#max-temp");
 
-// const day1 = $("#day1");
-// const day2 = $("#day2");
-// const day3 = $("#day3");
-// const day4 = $("#day4");
-// const day5 = $("#day5");
-// const day6 = $("#day6");
-// const day7 = $("#day7");
-
-// const selectorHours = [
-//   $("#hour1"), $("#hour2"), $("#hour3"), $("#hour4"), $("#hour5"),
-//   $("#hour6"), $("#hour7"), $("#hour8"), $("#hour9"), $("#hour10"),
-//   $("#hour11"), $("#hour12")
-// ];
 
 const showTime = () => {
-  const date = new Date();
-  let hours = date.getHours();
-  let minutes = date.getMinutes();
-  let seconds = date.getSeconds();
+  let d = new Date(); // (f) date
+  let date = d.getDate()  // get date
+  let day = d.getDay(); // get array day [0-6]
+
+  let hours = d.getHours(); // get hours
+  let minutes = d.getMinutes(); // get minute
   
-  hours = (hours < 10) ? "0" + hours : hours;
+  hours = (hours < 10) ? "0" + hours : hours; 
   minutes = (minutes < 10) ? "0" + minutes : minutes;
-  seconds = (seconds < 10) ? "0" + seconds : seconds;
 
-  // Format time with <span> for colons
-  let time = `${hours} <span class="colon">:</span> ${minutes} <span class="colon">:</span> ${seconds}`;
+  // format time with <span> for colons
+  let time = `${daysOfWeek[day]} ${date} <span> | </span> ${hours} <span class="colon">:</span> ${minutes}`;
   
-  localTime.html(time);
+  localTime.html(time); // render time to page
 
- // Apply blink effect to colons every second
- const currentSeconds = new Date().getSeconds();
-  if (currentSeconds % 1 === 0) {  // Blink every second
+
+ // apply blink effect to colons every second
+ const currentSeconds = new Date().getSeconds();  // get seconds
+  if (currentSeconds % 1 === 0) {  // blink every second
     $('.colon').addClass('blink');
   } else {
     $('.colon').removeClass('blink');
   }
 
-  setTimeout(showTime, 1000);
+  setTimeout(showTime, 1000); // delay 1s
 }
 
-showTime(); // Display local time
+showTime(); // display local time
 
 // API URLs
 const apiGeocoding = "https://api.opencagedata.com/geocode/v1/json";
 const keyGeocoding = "250178f6b314472c95280f5f73d608e1";
+
+
 const apiWeather = "https://api.openweathermap.org/data/2.5";
 const keyWeather = "d12f1c70ff84d00a509bf50060d7af7c";
 const apiHourlyWeather = "https://pro.openweathermap.org/data/2.5/forecast/hourly";
@@ -128,16 +113,101 @@ const forecastDays = () => {
 // Render geocoding
 const renderGeocoding = (data) => {
   cityName.text(data.results[0].components.city || data.results[0].components.county);
-  countryName.text(data.results[0].components.country);
+  countryCode.text(data.results[0].components.country_code.toUpperCase());
+
   forecastDays(); // Call function to display days
 };
 
 // Render current weather
 const renderCurrentWeather = (data) => {
-  weather.text(data.weather[0].main);
-  temp.text(data.main.temp + "\u00B0C");
-  humi.text(data.main.humidity + "%");
+  currentWeather.text(data.weather[0].main);  
+  currentTemp.text(Math.round(data.main.temp) + "\u00B0");
+  currentHumi.text(data.main.humidity + "%")
+  currentWindSpeed.text(data.wind.speed + " m/s")
+  
+  // load img by current weather
+  if (currentWeather.text() == "Thunderstorm") {
+    iconCurrentWeather.attr('src', 'assets/images/weather/thunderstorm/night-storm.webp');
+  } else if(currentWeather.text() == "Clouds"){
+    iconCurrentWeather.attr('src', 'assets/images/weather/cloudly/night-cloudly.webp');
+  } else if(currentWeather.text() == "Drizzle"){
+ iconCurrentWeather.attr('src', 'assets/images/weather/drizzle/night-drizzle.webp');
+  } else if(currentWeather.text() == "Rain"){
+ iconCurrentWeather.attr('src', 'assets/images/weather/rain/night-rain.webp');
+  } else if(currentWeather.text() == "Snow"){
+ iconCurrentWeather.attr('src', 'assets/images/weather/snow/night-snow.webp');
+  } else if(currentWeather.text() == "Clear"){
+    iconCurrentWeather.attr('src', 'assets/images/weather/snow/night-clear.webp');
+  }
 }
+
+
+// Render hourly forecast
+const renderHourlyForecast = (data) => {
+  const weatherData = data;
+  const currentTime = new Date();
+  const currentHour = currentTime.getHours();
+  const currentDateString = currentTime.toISOString().slice(0, 10);
+
+  
+  
+  const forecastContainer = document.querySelector('#hourly-forecast');
+  // Bersihkan konten lama
+  forecastContainer.innerHTML = '';
+  
+  const next12HoursDataToday = filterNext12HoursToday(weatherData, currentDateString, currentHour);
+
+  next12HoursDataToday.forEach((data) => {
+    const time = data.dt_txt.split(' ')[1].split(':').slice(0, 2).join(':');;
+    console.log(time);
+    
+    // const time = data.dt_txt.split(' ')[1].split(':')[0];
+    const temp = data.main.temp;
+   
+    // Membuat elemen cuaca baru secara di\amis
+    const forecastElement = `
+       <div class="bg-[#48319D]/80 w-12 h-24 rounded-full shadow-2xl mx-auto text-center">
+               <div class="flex justify-center items-center relative -top-5 ">
+                  <div class="mx-auto text-center">
+                     <img src="assets/images/weather/thunderstorm/night-storm.webp" alt="icon_weather"
+                        class="w-12 h-auto">
+                  </div>
+               </div>
+               <!--  -->
+               <div class="relative -top-3 text-xs text-white">
+                  <p class="">${time}</p>
+                  <p class="">${Math.round(temp)}&deg;</p>
+               </div>
+            </div>
+
+    `;
+
+     // Menyisipkan elemen baru ke dalam container
+     forecastContainer.innerHTML += forecastElement;
+    })
+  };
+  // next12HoursDataToday.forEach((data, index) => {
+  //   if (selectorHours[index]) {
+  //     const time = data.dt_txt.split(' ')[1].split(':')[0];
+  //     selectorHours[index].text(`Jam: ${time}, Suhu: ${data.main.temp}°C`);
+  //     selectorHours[index].show();
+  //   }
+  // });
+
+  // for (let i = next12HoursDataToday.length; i < selectorHours.length; i++) {
+  //   selectorHours[i].hide();
+  // }
+ 
+// Filter next 12 hours today
+const filterNext12HoursToday = (weatherData, currentDateString, currentHour) => {
+  const maxHourToday = Math.min(currentHour + 12, 24);
+
+  return weatherData.list.filter(data => {
+    const [date, time] = data.dt_txt.split(' ');
+    const [hour] = time.split(':');
+    return date === currentDateString && hour >= currentHour && hour < maxHourToday;
+  });
+};
 
 // Render daily forecast
 const renderDailyForecast = (data) => {
@@ -152,76 +222,39 @@ const renderDailyForecast = (data) => {
   day7.text(Math.round(data.list[6].temp.max) + "\u00B0C" + "/" + Math.round(data.list[6].temp.min) + "\u00B0C" + ", " + data.list[6].weather[0].main);
 }
 
-// Filter next 12 hours today
-const filterNext12HoursToday = (weatherData, currentDateString, currentHour) => {
-  const maxHourToday = Math.min(currentHour + 12, 24);
 
-  return weatherData.list.filter(data => {
-    const [date, time] = data.dt_txt.split(' ');
-    const [hour] = time.split(':');
-    return date === currentDateString && hour >= currentHour && hour < maxHourToday;
-  });
-};
+$(window).on("load", () => {
+  getCoordinates(
+    (lat, lon) => {
+      fetch(`${apiGeocoding}?q=${lat}%2C${lon}&key=${keyGeocoding}`)
+        .then((response) => response.json())
+        .then((responseJson) => renderGeocoding(responseJson))
+        .catch((error) => console.error("Error:", error));
 
-// Get minimum temperature from hourly data
-const getMinTempFromHourlyData = (hourlyData) => {
-  return Math.min(...hourlyData.map(data => data.main.temp));
-};
+      fetch(`${apiWeather}/weather?lat=${lat}&lon=${lon}&appid=${keyWeather}&units=metric`)
+        .then((response) => response.json())
+        .then((responseJson) => renderCurrentWeather(responseJson))
+        .catch((error) => console.error("Error:", error));
+ 
+        /*
+      fetch(`${apiWeather}/forecast/daily?lat=${lat}&lon=${lon}&cnt=7&appid=${keyWeather}&units=metric`)
+        .then((response) => response.json())
+        .then((responseJson) => renderDailyForecast(responseJson))
+        .catch((error) => console.error("Error:", error));
+ */
+      fetch(`${apiHourlyWeather}?lat=${lat}&lon=${lon}&appid=${keyWeather}&units=metric`)
+        .then((response) => response.json())
+        .then((responseJson) => renderHourlyForecast(responseJson))
+        .catch((error) => console.error("Error:", error));
 
-// Render hourly forecast
-const renderHourlyForecast = (data) => {
-  const weatherData = data;
-  const currentTime = new Date();
-  const currentHour = currentTime.getHours();
-  const currentDateString = currentTime.toISOString().slice(0, 10);
 
-  const next12HoursDataToday = filterNext12HoursToday(weatherData, currentDateString, currentHour);
-  const minTempToday = getMinTempFromHourlyData(next12HoursDataToday);
-  console.log("Min temp today from hourly data:", minTempToday);
-  minTemp.text(Math.round(minTempToday) + "\u00B0C"); 
-
-  next12HoursDataToday.forEach((data, index) => {
-    if (selectorHours[index]) {
-      const time = data.dt_txt.split(' ')[1].split(':')[0];
-      selectorHours[index].text(`Jam: ${time}, Suhu: ${data.main.temp}°C`);
-      selectorHours[index].show();
+    },
+    (err) => {
+      alert("Unable to retrieve your location. Please ensure location services are enabled.");
+      console.error("Geolocation error:", err);
     }
-  });
+  );
+});
 
-  for (let i = next12HoursDataToday.length; i < selectorHours.length; i++) {
-    selectorHours[i].hide();
-  }
-}
-
-// On window load
-// $(window).on("load", () => {
-//   getCoordinates(
-//     (lat, lon) => {
-//       fetch(`${apiGeocoding}?q=${lat}%2C${lon}&key=${keyGeocoding}`)
-//         .then((response) => response.json())
-//         .then((responseJson) => renderGeocoding(responseJson))
-//         .catch((error) => console.error("Error:", error));
-
-//       fetch(`${apiWeather}/weather?lat=${lat}&lon=${lon}&appid=${keyWeather}&units=metric`)
-//         .then((response) => response.json())
-//         .then((responseJson) => renderCurrentWeather(responseJson))
-//         .catch((error) => console.error("Error:", error));
-
-//       fetch(`${apiWeather}/forecast/daily?lat=${lat}&lon=${lon}&cnt=7&appid=${keyWeather}&units=metric`)
-//         .then((response) => response.json())
-//         .then((responseJson) => renderDailyForecast(responseJson))
-//         .catch((error) => console.error("Error:", error));
-
-//       fetch(`${apiHourlyWeather}?lat=${lat}&lon=${lon}&appid=${keyWeather}&units=metric`)
-//         .then((response) => response.json())
-//         .then((responseJson) => renderHourlyForecast(responseJson))
-//         .catch((error) => console.error("Error:", error));
-//     },
-//     (err) => {
-//       alert("Unable to retrieve your location. Please ensure location services are enabled.");
-//       console.error("Geolocation error:", err);
-//     }
-//   );
-// });
 
 
